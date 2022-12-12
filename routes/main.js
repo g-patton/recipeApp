4772a331297289f5edf2c069799029c604f67da4
Graham Patton <graham.m.patton@gmail.com>
2022-12-12 15:32:41 -0500

    recipe app working other than feed route

--- routes/main.js
+++ routes/main.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
+const recipesController = require("../controllers/recipe");
 const { ensureAuth } = require("../middleware/auth");
 
 //Main Routes 
 router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/profile", ensureAuth, recipesController.getProfile);
+router.get("/favorites", ensureAuth, recipesController.getFavorites);
 
 //Routes for user login/signup
 router.get("/login", authController.getLogin);
